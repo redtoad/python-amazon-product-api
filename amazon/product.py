808dcc81976755911ffd80dc429e8fcc9b23b86e
Sebastian Rahlf <redtoad@users.noreply.github.com>
2009-09-22 13:01:09 +0200

    Docs added.

--- amazon/product.py
+++ amazon/product.py
@@ -3,17 +3,17 @@
 Amazon Product Advertising API
 ==============================
 
-The Product Advertising API provides programmatic access to Amazon's product
-selection and discovery functionality so that developers like you can advertise
-Amazon products to monetize your website.
-
-The Product Advertising API helps you advertise Amazon products using product
-search and look up capability, product information and features such as
-Customer Reviews, Similar Products, Wish Lists and New and Used listings. You
-can make money using the Product Advertising API to advertise Amazon products
-in conjunction with the Amazon Associates program. Be sure to join the Amazon
-Associates program to earn up to 15% in referral fees when the users you refer
-to Amazon sites buy qualifying products.
+    The Product Advertising API provides programmatic access to Amazon's
+    product selection and discovery functionality so that developers like you
+    can advertise Amazon products to monetize your website.
+    
+    The Product Advertising API helps you advertise Amazon products using
+    product search and look up capability, product information and features
+    such as Customer Reviews, Similar Products, Wish Lists and New and Used
+    listings. You can make money using the Product Advertising API to advertise
+    Amazon products in conjunction with the Amazon Associates program. Be sure
+    to join the Amazon Associates program to earn up to 15% in referral fees
+    when the users you refer to Amazon sites buy qualifying products.  
 
 More info can be found at
 https://affiliate-program.amazon.com/gp/advertising/api/detail/main.html
@@ -24,12 +24,14 @@ Requirements
 You need an Amazon Webservice account.
 
 Kudos
-------
+-----
 
 The ``_build_url()`` function is based on code by Adam Cox (found at 
 http://blog.umlungu.co.uk/blog/2009/jul/12/pyaws-adding-request-authentication/)
 
 """
+
+__docformat__ = "restructuredtext en"
 
 LOCALES = {
     'ca' : 'http://ecs.amazonaws.ca/onca/xml', 
@@ -92,7 +94,26 @@ class InvalidResponseGroup (Exception):
 
 class ProductAdvertisingAPI (object):
     
-    VERSION = '2009-07-01'
+    """
+    Wrapper class for the Amazon Product Advertising API. You will need both an 
+    AWS access key id and the secret counterpart.
+    
+    Example::
+    
+        AWS_KEY = '...'
+        SECRET_KEY = '...'
+        
+        api = ProductAdvertisingAPI(AWS_KEY, SECRET_KEY)
+        root = api.item_lookup('987311264224', IdType='ISBN', 
+                    SearchIndex='Books', ResponseGroup='Reviews', ReviewPage=1)
+        
+        rating = root.Items.Item.CustomerReviews.AverageRating.pyval
+        total_reviews = root.Items.Item.CustomerReviews.TotalReviews.pyval
+        review_pages = root.Items.Item.CustomerReviews.TotalReviewPages.pyval
+
+    """
+    
+    VERSION = '2009-07-01' #: supported Amazon API version
     
     def __init__(self, access_key_id, secret_access_key, locale='de'):
         
@@ -149,8 +170,8 @@ class ProductAdvertisingAPI (object):
         tree = objectify.parse(urlopen(url))
         root = tree.getroot()
         
-        #from lxml import etree
-        #print etree.tostring(tree, pretty_print=True)
+        #~ from lxml import etree
+        #~ print etree.tostring(tree, pretty_print=True)
         
         nspace = root.nsmap.get(None, '')
         errors = root.xpath('//aws:Request/aws:Errors/aws:Error', 
@@ -159,8 +180,8 @@ class ProductAdvertisingAPI (object):
         for error in errors:
             raise AWSError(error.Code.text, error.Message.text)
         
-        #from lxml import etree
-        #print etree.tostring(root, pretty_print=True)
+        #~ from lxml import etree
+        #~ print etree.tostring(root, pretty_print=True)
         return root
     
     def item_lookup(self, id, **params):
