4095ac4ce37242cd8816244ec0d3c8dfc461c525
basti <basti@localhost>
2009-11-20 14:16:16 +0100

    Trove classifiers completed.

--- setup.py
+++ setup.py
@@ -14,15 +14,21 @@ setup(
     
     description = 'A Python wrapper for the Amazon Product Advertising API.', 
     long_description=read('README.rst'),
-    keywords = 'amazon product advertising api wrapper',
+    keywords = 'amazon product advertising api wrapper signed requests',
     
     py_modules = ['amazonproduct'],
     install_requires=['lxml>=2.1.5'],
     
     classifiers = [
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP',
+        'Operating System :: OS Independent', 
+        'Development Status :: 3 - Alpha', 
+        'Intended Audience :: Developers', 
+        'License :: OSI Approved :: BSD License', 
+        'Operating System :: OS Independent', 
+        'Programming Language :: Python :: 2.4', 
+        'Programming Language :: Python :: 2.5', 
+        'Programming Language :: Python :: 2.6', 
+        'Topic :: Internet :: WWW/HTTP', 
         'Topic :: Software Development :: Libraries :: Python Modules',
-    ],
+    ]
 )
