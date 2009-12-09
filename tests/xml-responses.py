15d57c40bac0ca98740791a09effed00c14beaa3
Sebastian Rahlf <redtoad@users.noreply.github.com>
2009-12-09 19:02:11 +0100

    XML files for subsequent API calls by the same CustomAPI instance are stored separately.

--- tests/xml-responses.py
+++ tests/xml-responses.py
@@ -5,7 +5,7 @@ from StringIO import StringIO
 import unittest
 import urllib2
 
-from amazonproduct import API
+from amazonproduct import API, ResultPaginator
 from amazonproduct import AWSError
 from amazonproduct import InvalidParameterValue, InvalidListType
 from amazonproduct import InvalidSearchIndex, InvalidResponseGroup
@@ -31,15 +31,28 @@ class CustomAPI (API):
     
     """
     Uses stored XML responses from local files (or retrieves them from Amazon 
-    if they are not present yet).
+    if they are not present yet). The number of calls via a particular API 
+    instance is tracked and local files are named accordingly.
     """
+    
+    def __init__(self, *args, **kwargs):
+        super(CustomAPI, self).__init__(*args, **kwargs)
+        self.calls = 0
     
     def _call(self, url):
         """
         Uses XML response from (or stores in) local file.
         """
         
-        if not os.path.exists(self.local_file) or OVERWRITE_TESTS:
+        # subsequent calls of this API instance
+        # will be stored in different files
+        self.calls += 1
+        path = self.local_file
+        if self.calls > 1:
+            head, tail = os.path.splitext(self.local_file)
+            path = head + '-%i' % self.calls + tail
+        
+        if not os.path.exists(path) or OVERWRITE_TESTS:
             tree = objectify.parse(API._call(self, url))
             root = tree.getroot()
             
@@ -49,20 +62,21 @@ class CustomAPI (API):
                                   namespaces={'aws' : nspace}):
                 if arg.get('Name') in 'AWSAccessKeyId Signature':
                     arg.set('Value', 'X'*15)
-            
+                    
             data = etree.tostring(root, pretty_print=True)
-            local_dir = os.path.dirname(self.local_file)
+            
+            local_dir = os.path.dirname(path)
             if not os.path.exists(local_dir):
                 #print 'creating %s...' % local_dir
                 os.mkdir(local_dir)
                 
-            fp = open(self.local_file, 'wb')
+            fp = open(path, 'wb')
             #print 'storing response in %s...' % self.local_file 
             fp.write(data)
             fp.close()
             return StringIO(data)
             
-        return open(self.local_file, 'rb')
+        return open(path, 'rb')
 
 
 class XMLResponseTestCase (unittest.TestCase):
@@ -89,7 +103,7 @@ class XMLResponseTestCase (unittest.TestCase):
         version = self.api.VERSION
         klass = re.search(r'(\w+?)Test(Case)?', self.__class__.__name__).group(1)
         method = re.search(r'test_?(\w+)', self._testMethodName).group(1)
-        method = method.replace('_', '-') 
+        method = method.replace('_', '-')
         return '%s/%s-%s.xml' % (version, klass, method)
     
 
