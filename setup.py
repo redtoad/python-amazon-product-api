15caaeb79a41bab39c5f280a6e9e06319e8f47c1
basti <basti@localhost>
2010-06-28 09:20:42 +0200

    Requirements are now Python2.4 friendly.

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python
 
-from setuptools import setup
 import os
+from setuptools import setup
 import sys
 
 import amazonproduct
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+reqs = []
+ # for python2.4
+if sys.version_info[:2] < (2, 5):
+    reqs += ['pycrypto']
 
 setup(
     name = 'python-amazon-product-api',
@@ -16,28 +21,28 @@ setup(
     author_email = 'basti AT redtoad DOT de',
     url="http://bitbucket.org/basti/python-amazon-product-api/downloads/",
     license='bsd',
-    
-    description = 'A Python wrapper for the Amazon Product Advertising API.', 
+
+    description = 'A Python wrapper for the Amazon Product Advertising API.',
     long_description=read('README'),
     keywords = 'amazon product advertising api wrapper signed requests',
-    
+
     py_modules = ['amazonproduct'],
-    install_requires=['lxml>=2.1.5'], # 'pycrypto' # for python2.4
+    install_requires=reqs,
 
     test_suite = 'tests',
     test_loader = 'tests:XMLResponseTestLoader',
-    tests_require = 'nose',
+    tests_require = ['nose', 'lxml>=2.1.5'],
 
     classifiers = [
-        'Operating System :: OS Independent', 
-        'Development Status :: 3 - Alpha', 
-        'Intended Audience :: Developers', 
-        'License :: OSI Approved :: BSD License', 
-        'Operating System :: OS Independent', 
-        'Programming Language :: Python :: 2.4', 
-        'Programming Language :: Python :: 2.5', 
-        'Programming Language :: Python :: 2.6', 
-        'Topic :: Internet :: WWW/HTTP', 
+        'Operating System :: OS Independent',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Internet :: WWW/HTTP',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
