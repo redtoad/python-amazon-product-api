4427b2013d2600630dedcec1343ff30570fb7f68
Sebastian Rahlf <basti@redtoad.de>
2011-09-12 08:31:14 +0200

    Added minidom processor.

--- amazonproduct/processors.py
+++ amazonproduct/processors.py
@@ -3,6 +3,8 @@
 Contains all result processors.
 """
 
+import xml.dom.minidom
+
 from amazonproduct.errors import AWSError
 from amazonproduct.paginators import LxmlItemSearchPaginator
 
@@ -67,3 +69,21 @@ class LxmlObjectifyProcessor (object):
         return root
 
     item_search_paginator = LxmlItemSearchPaginator
+
+
+class MinidomProcessor(object):
+
+    """
+    Alternative result parser using ``xml.dom.minidom` from the standard
+    library.
+    """
+
+    def __call__(self, fp):
+        root = xml.dom.minidom.parse(fp)
+        # parse errors
+        for error in root.getElementsByTagName('Error'):
+            code = error.getElementsByTagName('Code')[0].firstChild.nodeValue
+            msg = error.getElementsByTagName('Message')[0].firstChild.nodeValue
+            raise AWSError(code, msg)
+        return root
+
