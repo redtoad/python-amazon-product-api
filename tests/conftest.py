0486323cb5d3840dee9a9e8c9a3de7c16c45a23c
Sebastian Rahlf <redtoad@users.noreply.github.com>
2011-09-12 08:55:12 +0200

    Grouped py.test custom options and fixed skip messages.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,14 @@
 
 def pytest_addoption(parser):
-    parser.addoption('--locale', action='append', dest='locales',
+    group = parser.getgroup('amazonproduct',
+        'custom options for testing python-amazon-product-api')
+    group._addoption('--locale', action='append', dest='locales',
         metavar='LOCALE', help='Locale to use (e.g. "de" or "us"). This option '
-            'can be used more than once.')
-    parser.addoption('--api-version', action='append', dest='versions',
+            'can be used more than once. Note that tests with specific locales '
+            'defined which do not match the ones specified by this option will '
+            'NOT be run.')
+    group._addoption('--api-version', action='append', dest='versions',
         metavar='VERSION', help='API version to use (e.g. "2010-09-01"). This '
-            'option can be used more than once.')
+            'option can be used more than once. Note that tests with specific '
+            'versions defined which do not match the ones specified by this '
+            'option will NOT be run.')
