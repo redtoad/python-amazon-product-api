d5d12f59cb74b99fd923b4c0195b356485855347
Sebastian Rahlf <basti@redtoad.de>
2011-12-02 20:29:49 +0100

    Adjusted cart operations tests.

--- tests/utils.py
+++ tests/utils.py
@@ -1,22 +1,12 @@
 
-from lxml import etree, objectify
-import os.path
+from lxml import objectify
 import re
-from StringIO import StringIO
-import urllib2
 
 try: # make it python2.4/2.5 compatible!
     from urlparse import urlparse, parse_qs
 except ImportError: # pragma: no cover
     from urlparse import urlparse
     from cgi import parse_qs
-
-from amazonproduct.api import API
-from amazonproduct.errors import AWSError
-
-from tests import XML_TEST_DIR, TESTABLE_API_VERSIONS, TESTABLE_LOCALES
-from tests import AWS_KEY, SECRET_KEY 
-
 
 def convert_camel_case(operation):
     """
@@ -32,82 +22,6 @@ def extract_operations_from_wsdl(path):
     wsdlns = 'http://schemas.xmlsoap.org/wsdl/'
     return set(root.xpath('//ws:operation/@name', namespaces={'ws' : wsdlns}))
 
-
-class Cart (object):
-
-    """
-    Convenience class for testing XML responses of CartXXX operations..
-    """
-
-    class Item (object):
-
-        def __repr__(self):
-            return '<Item %ix %s (=%s %s)>' % (
-                self.quantity, self.asin, self.price[0], self.price[1])
-
-        @staticmethod
-        def from_xml(node):
-            """
-            Takes <CartItem> node as ``lxml.objectify``d element and returns a 
-            CartItem instance.
-            """
-            item = Cart.Item()
-            item.item_id = node.CartItemId.pyval
-            item.asin = node.ASIN.pyval
-            item.seller = node.SellerNickname.pyval
-            item.quantity = node.Quantity.pyval
-            item.title = node.Title.pyval
-            item.product_group = node.ProductGroup.pyval
-            item.price = (
-                node.Price.Amount.pyval, 
-                node.Price.CurrencyCode.pyval)
-            item.total = (
-                node.ItemTotal.Amount.pyval, 
-                node.ItemTotal.CurrencyCode.pyval)
-            return item
-
-    def __init__(self):
-        self.items = []
-
-    def __getitem__(self, key):
-        for item in self.items:
-            if key in (item.asin, item.item_id):
-                return item
-        raise IndexError(key)
-
-    def __len__(self):
-        return sum([item.quantity for item in self.items])
-
-    def __repr__(self):
-        if self.subtotal is None:
-            return '<Cart %s %s>' % (self.cart_id, self.items)
-        return '<Cart %s %s %.2f %s>' % (self.cart_id, self.items, 
-            self.subtotal[0]/100.0, self.subtotal[1])
-
-    def get_itemid_for_asin(self, asin):
-        for item in self.items:
-            if asin == item.asin:
-                return item.item_id
-        return None
-
-    @staticmethod
-    def from_xml(node):
-        """
-        Takes <Cart> node as ``lxml.objectify``d element and returns a Cart
-        instance.
-        """
-        cart = Cart()
-        cart.cart_id = node.CartId.pyval
-        cart.hmac = node.HMAC.pyval
-        try:
-            for item_node in node.CartItems.CartItem:
-                cart.items.append(Cart.Item.from_xml(item_node))
-            cart.url = node.PurchaseURL.pyval
-            cart.subtotal = (node.SubTotal.Amount, node.SubTotal.CurrencyCode)
-        except AttributeError:
-            cart.url = None
-            cart.subtotal = None
-        return cart
 
 #: list of changeable and/or sensitive (thus ignorable) request arguments
 IGNORABLE_ARGUMENTS = ('Signature', 'AWSAccessKeyId', 'Timestamp', 'AssociateTag')
