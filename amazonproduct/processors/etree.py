90e573f9ef20017ff137996e6bf8c665b41561b0
Sebastian Rahlf <redtoad@users.noreply.github.com>
2013-06-05 23:18:21 +0200

    Cleaned up lxml processors.

--- amazonproduct/processors/etree.py
+++ amazonproduct/processors/etree.py
@@ -7,57 +7,11 @@ from lxml import etree
 
 from amazonproduct.contrib.cart import Cart, Item
 from amazonproduct.errors import AWSError
-from amazonproduct.processors import BaseResultPaginator, BaseProcessor
+from amazonproduct.processors import BaseProcessor
 from amazonproduct.processors import ITEMS_PAGINATOR, RELATEDITEMS_PAGINATOR
 
-
-class XPathPaginator (BaseResultPaginator):
-
-    """
-    Result paginator using XPath expressions to extract page and result
-    information from XML.
-    """
-
-    counter = current_page_xpath = total_pages_xpath = total_results_xpath = None
-
-    def paginator_data(self, root):
-        nspace = root.nsmap.get(None, '')
-        def fetch_value(xpath, default):
-            try:
-                import lxml.etree; print lxml.etree.tostring(root, pretty_print=True)
-                node = root.xpath(xpath, namespaces={'aws': nspace})[0]
-                return int(node.text)
-            except AttributeError:
-                return int(root)
-            except IndexError:
-                return default
-        return map(lambda a: fetch_value(*a), [
-            (self.current_page_xpath, 1),
-            (self.total_pages_xpath, 0),
-            (self.total_results_xpath, 0)
-        ])
-
-    def iterate(self, root):
-        nspace = root.nsmap.get(None, '')
-        return root.xpath(self.items, namespaces={'aws': nspace})
-
-
-class SearchPaginator (XPathPaginator):
-
-    counter = 'ItemPage'
-    current_page_xpath = '//aws:Items/aws:Request/aws:ItemSearchRequest/aws:ItemPage'
-    total_pages_xpath = '//aws:Items/aws:TotalPages'
-    total_results_xpath = '//aws:Items/aws:TotalResults'
-    items = '//aws:Items/aws:Item'
-
-
-class RelatedItemsPaginator (XPathPaginator):
-
-    counter = 'RelatedItemPage'
-    current_page_xpath = '//aws:RelatedItemPage'
-    total_pages_xpath = '//aws:RelatedItems/aws:RelatedItemPageCount'
-    total_results_xpath = '//aws:RelatedItems/aws:RelatedItemCount'
-    items = '//aws:RelatedItems/aws:RelatedItem/aws:Item'
+from amazonproduct.processors._lxml import SearchPaginator
+from amazonproduct.processors._lxml import RelatedItemsPaginator
 
 
 class Processor (BaseProcessor):
